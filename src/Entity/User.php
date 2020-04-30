afe0b6595ace062af9754cc307a15aaaba3512d7
Florian Moser <products@famoser.ch>
2020-04-30 12:18:24 +0200

    add default value

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -117,7 +117,7 @@ class User extends BaseEntity
      *
      * @ORM\Column(type="integer", options={"default" : 0})
      */
-    private $outOfOpeningTimesDiscount;
+    private $outOfOpeningTimesDiscount = 0;
 
     /**
      * this is in cents.
